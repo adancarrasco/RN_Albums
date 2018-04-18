94d1980e6e9d2762dd00d5ee4ece79dd27141d05
Adan Carrasco <adan.carrasco@hotmail.com>
2018-04-18 17:49:47 -0600

    Add AlbumListComponent.

--- App.js
+++ App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Header />
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Header headerTitle={'Albums!'} />
+        <AlbumList />
       </View>
     );
   }
