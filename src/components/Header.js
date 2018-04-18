04c2fee5b62f816f49b764d21fdad658b1e913e2
Adan Carrasco <adan.carrasco@hotmail.com>
2018-04-18 17:49:35 -0600

    Add Header Styles.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const Header = () => {
-  const {textStyle} = styles;
-  return <Text style={textStyle}>Albums!</Text>;
+const Header = props => {
+  const {textStyle, viewStyle} = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerTitle}</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
   textStyle: {
     fontSize: 20,
   },
