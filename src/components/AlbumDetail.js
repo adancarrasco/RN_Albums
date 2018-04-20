580a9ad359d3a3aef88dc1accccf17e44605a987
Adan Carrasco <adan.carrasco@hotmail.com>
2018-04-19 20:08:36 -0600

    1. Add thumbnail 2. Add text for title and artist 3. Add album cover image 4. Add Button to open the browser using Linking

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,65 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = props => {
-  const {album} = props;
+const AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image, image, url} = album;
+  const {headerContentStyle, imageContainerStyle} = styles;
+  const {
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    textContainerStyle,
+  } = headerContentStyle;
   return (
     <Card>
       <CardSection>
-        <Text>{album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{uri: thumbnail_image}} />
+        </View>
+        <View style={textContainerStyle}>
+          <Text style={textContainerStyle.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image style={imageContainerStyle} source={{uri: image}} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    textContainerStyle: {
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      headerTextStyle: {
+        fontSize: 18,
+      },
+    },
+    thumbnailStyle: {
+      height: 50,
+      width: 50,
+    },
+    thumbnailContainerStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 10,
+      marginRight: 10,
+    },
+  },
+  imageContainerStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
 };
 
 export default AlbumDetail;
