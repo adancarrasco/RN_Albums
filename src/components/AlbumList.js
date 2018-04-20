30f71ae5fa5dacf561fc8681a81be39670ec5ed0
Adan Carrasco <adan.carrasco@hotmail.com>
2018-04-19 18:04:41 -0600

    Create AlbumDetail, Card and CardSection, update AlbumList to use Card and CardSection for AlbumDetail.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import Axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends React.Component {
+  state = {
+    albums: [],
+  };
+
   UNSAFE_componentWillMount() {
     console.log('component');
+
+    const self = this;
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(
+      response => {
+        self.setState({
+          albums: response.data,
+        });
+      }
+    );
+  }
+
+  renderAlbumns() {
+    return this.state.albums.map((album, index) => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
   }
 
   render() {
-    return (
-      <View>
-        <Text>Album List</Text>
-      </View>
-    );
+    return <View>{this.renderAlbumns()}</View>;
   }
 }
 
